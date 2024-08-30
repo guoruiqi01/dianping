dbfdbbae9456e2f5f20dbf1dd0552962773d78f4
ruiqi guo <1811319761@qq.com>
2024-08-30 10:55:57 +0800

    实现查看探店笔记

--- src/main/java/com/hmdp/service/IBlogService.java
+++ src/main/java/com/hmdp/service/IBlogService.java
@@ -1,5 +1,6 @@
 package com.hmdp.service;
 
+import com.hmdp.dto.Result;
 import com.hmdp.entity.Blog;
 import com.baomidou.mybatisplus.extension.service.IService;
 
@@ -13,4 +14,17 @@ import com.baomidou.mybatisplus.extension.service.IService;
  */
 public interface IBlogService extends IService<Blog> {
 
+    /**
+     * 查询hot
+     * @param current
+     * @return
+     */
+    Result queryHotBlog(Integer current);
+
+    /**
+     * 根据id查询blog
+     * @param id
+     * @return
+     */
+    Result queryBlogById(Long id);
 }
